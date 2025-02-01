d28df4b7f9df80e68fc2de40189da714aaf4f0b0
Ehsan Ahmadvand <ehsan.ahmadvand@samim.net>
2025-02-01 08:20:51 +0330

    Implementing a Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,29 @@
 export default class Queue<T> {
     public length: number;
-
-    
+    private items: T[];
 
     constructor() {
+        this.length = 0;
+        this.items = [];
     }
 
     enqueue(item: T): void {
+        this.items.push(item);
+        this.length++;
+    }
 
-}
     deque(): T | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
+        this.length--;
+        return this.items.shift();
+    }
 
-}
     peek(): T | undefined {
-
-}
+        if (this.length === 0) {
+            return undefined;
+        }
+        return this.items[0];
+    }
 }
